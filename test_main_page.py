1555d67d1aac2477e417358ec92f5ebb01c54f53
chmaxims <max.chesn@gmail.com>
2025-10-03 02:02:12 +0300

    add class TestLoginFromMainPage and fixture login_quest

--- test_main_page.py
+++ test_main_page.py
@@ -8,20 +8,21 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 import time
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/'
+        page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open() # открываем страницу
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url) # выполняем метод страницы - переходим на страницу логина
+        login_page.should_be_login_page()
 
-def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open() # открываем страницу
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url) # выполняем метод страницы - переходим на страницу логина
-    login_page.should_be_login_page()
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/"
